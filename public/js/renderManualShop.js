a10d7394f9970a3ed397652f3fb8cb3102cc7f08
Alexis <amaryllisno@gmail.com>
2026-07-02 22:09:31 +0200

    cors

--- public/js/renderManualShop.js
+++ public/js/renderManualShop.js
@@ -5,6 +5,7 @@ const carouselState = new Map();
 const DEFAULT_ASPECT_RATIO = '4 / 5';
 const DEFAULT_EMAILER_ENDPOINT =
   'https://emailer-withered-snow-9611.fly.dev/send-email';
+const CORS_API_HOST = 'https://cors-anywhere.herokuapp.com/';
 
 const elementIds = {
   products: 'shop-products',
@@ -31,6 +32,8 @@ const toEmailLineBreaks = (value) =>
   escapeHtml(value).replace(/\r?\n/g, '<br>');
 const getEmailerEndpoint = () =>
   shopConfig.emailerEndpoint || DEFAULT_EMAILER_ENDPOINT;
+const withCorsProxy = (url) =>
+  url.startsWith(CORS_API_HOST) ? url : `${CORS_API_HOST}${url}`;
 const getOrderPrice = (product) => {
   if (
     product.status === 'on sale' &&
@@ -427,7 +430,7 @@ const sendOrderInquiry = async () => {
   const senderEmail = getInputValue('email');
   const summary = buildOrderSummaryHtml();
   const subject = 'Painting order inquiry';
-  const endpoint = getEmailerEndpoint();
+  const endpoint = withCorsProxy(getEmailerEndpoint());
 
   try {
     const response = await fetch(endpoint, {
